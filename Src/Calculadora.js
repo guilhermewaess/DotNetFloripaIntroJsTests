ab50cc1f005e3b314e54a50b394d9fca6cfbcbdf
Guilherme Waess <guilherme.waess@gmail.com>
2015-09-30 16:53:47 -0300

    teste calculadora

--- Src/Calculadora.js
+++ Src/Calculadora.js
@@ -5,6 +5,11 @@ define([
 	var calculadora = function () {
 
 	}
+	
+	calculadora.prototype.some = function (num1, num2) {
+		console.log(num1, num2);
+		return num1 + num2;
+	}
 
 	return calculadora;
 });
--- Tests/CalculadoraSpec.js
+++ Tests/CalculadoraSpec.js
@@ -1,7 +1,13 @@
 define([
 	'Src/Calculadora'
-], function(Calculadora) {
+], function (Calculadora) {
 	'use strict';
-	
-	
+	var calculadora;
+
+	describe('Dada uma calculadora', function () {
+		it('deve somar num1 com num2', function () {
+			calculadora = new Calculadora();
+			expect(calculadora.some(1, 1)).toEqual(2)
+		});
+	});
 });
